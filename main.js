840db1dfca384f633ca78bdd5a2912c52683f3a9
Roybeeezee <roy.burksjr@gmail.com>
2026-08-14 11:05:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1320,
+    height: 880,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'UQC v1.2.0',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.setMenuBarVisibility(false);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
